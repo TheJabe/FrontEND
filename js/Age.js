3f566f200f74d6d5941f598f385125d94927e174
TheJabe <iluha.kom@gmail.com>
2019-03-18 00:35:58 +0400

    Alerts js фичи

---
+++ js/Age.js
@@ -0,0 +1,9 @@
+var year;
+var age=prompt('Сколько вам лет?');
+if(age >= 30){
+    year='Старость не радость, всех это ждет';
+}
+else {
+    year='Везёт тебе';
+}
+alert (year);
--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 alert("Welcome");
 var i=101;
 var b=10;
+
+a= i + b;
+alert(a);
+
+var c=null;
+summ= c + i;
+alert(summ);
+
+var e=undefined;
+nothing= e + b;
+alert(nothing);
+
+var date = prompt("С какого дня начинается неделя?");
+if (date === 'понедельник') {
+    alert('Правильно, до пятницы далеко)')
+}
+else{
+    alert('Не правильно, видимо кто-то в Америке живет;)')
+}
+
+
